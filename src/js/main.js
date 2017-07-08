f83de7e67da620ed797ed20e51b0db8b6fe68f65
DragonDark13 <alekz.zak@mail.ru>
2017-07-08 11:31:55 +0300

    effects, hover, focus

--- src/js/main.js
+++ src/js/main.js
@@ -10,4 +10,17 @@ jQuery(document).ready(function(jQuery) {
             jQuery('body').prepend('<div id="size" style="background:#000;padding:5px;position:fixed;z-index:9999;color:#fff;">Width = '+myWidth+'</div>');
         });
 
+		jQuery('.meni_list a').click(function(){
+			var target = jQuery(this).attr('href');
+			jQuery('html, body').animate({scrollTop: jQuery(target).offset().top}, 1000);
+			return false;
+		});
+
+		jQuery(".plan .my_btn ").hover(function() {
+			jQuery(this).closest('.plan').addClass('active');
+		}, function() {
+			/* Stuff to do when the mouse leaves the element */
+			jQuery(this).closest('.plan').removeClass('active');
+		});
+
 });
